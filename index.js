90c7ce8971a3ca1b8ad70aa0ffd22bc179d48d0e
FabricioRubim <fabriciofraga200618@gmail.com>
2025-08-11 23:12:08 -0300

    Mudança no codigo novamente

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let vidaPerdida = 20;
 if (vidaDoInimigo <= vidaPerdida) {
 
     console.log(`O ${nomeDoHeroi} atacou o ${inimigo}`);
-    console.log(`O inimigo perdeu ${vidaPerdida}`);
+    console.log(`O inimigo perdeu ${vidaPerdida} de vida`);
 
 }
 
@@ -21,32 +21,31 @@ console.log(`${nomeDoHeroi} venceu do ${inimigo}`);
 nivel += 5000;
 console.log(`O ${nomeDoHeroi} ganhou ${nivel} de xp`);
 
-
 if (nivel <= 1000) {
     console.log("Ferro");
 }
 
-else if (nivel >= 1001 && 2000) {
+else if (nivel >= 1001 && nivel <= 2000) {
     console.log("Bronze");
 }
 
-else if (nivel >= 2001 && 5000) {
+else if (nivel >= 2001 && nivel <= 5000) {
     console.log("Prata");
 }
 
-else if (nivel >= 5001 && 7000) {
+else if (nivel >= 5001 && nivel <= 7000) {
     console.log("Ouro");
 }
 
-else if (nivel >= 7001 && 8000) {
+else if (nivel >= 7001 && nivel <= 8000) {
     console.log("Platina diamante");
 }
 
-else if (nivel >= 8001 && 9000) {
+else if (nivel >= 8001 && nivel <= 9000) {
     console.log("Ascendente");
 }
 
-else if (nivel >= 9001 && 10000) {
+else if (nivel >= 9001 && nivel <= 10000) {
     console.log("Imortal");
 }
 
@@ -54,4 +53,4 @@ else if (nivel >= 10001) {
     console.log("Radiante");
 }
 
-console.log(`${nomeDoHeroi} chegou ao nivel Ouro`);
+console.log(`${nomeDoHeroi} chegou ao nivel ${nivel}`);
